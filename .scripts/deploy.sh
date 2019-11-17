93fc94bc8549bcfe574a0015814ce416f815b560
Damian F <damian.fikier@wp.pl>
2019-11-17 23:25:47 +0100

    env #3

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,22 +6,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   cd "$DIR/.." # Go to project dir.
 
   ssh -v $DROPLET_USER@$DROPLET_IP -o StrictHostKeyChecking=no <<-EOF
-    echo '$POSTGRES_DB'
-    echo $POSTGRES_DB
-    export POSTGRES_DB='$POSTGRES_DB'
-    export POSTGRES_USER='$POSTGRES_USER'
-    export POSTGRES_PWD='$POSTGRES_PWD'
-    export PGADMIN_PORT=$PGADMIN_PORT
-    export PGADMIN_LOGIN='$PGADMIN_LOGIN'
-    export PGADMIN_PWD='$PGADMIN_PWD'
     cd $SSH_PROJECT_FOLDER
     git pull
     echo "export POSTGRES_DB=$POSTGRES_DB"
     echo "export POSTGRES_USER=$POSTGRES_USER"
     echo "export POSTGRES_PWD=$POSTGRES_PWD"
     echo "export PGADMIN_PORT=$PGADMIN_PORT"
-    echo "export PGADMIN_LOGIN=$PGADMIN_LOGIN"
-    echo "export PGADMIN_PWD=$PGADMIN_PWD"
+    echo "export PGADMIN_LOGIN='$PGADMIN_LOGIN'"
+    echo "export PGADMIN_PWD='$PGADMIN_PWD'"
     echo "export JWT_SECRET_KEY=$JWT_SECRET_KEY"
     echo "export POSTGRES_PORT=5432"
     echo "export POSTGRES_HOST=postgres"
