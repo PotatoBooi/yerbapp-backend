83e6912e3d1b825d8711d019d1cffbf5080e7df9
Damian F <damian.fikier@wp.pl>
2019-11-17 23:47:28 +0100

    config issues...

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -20,6 +20,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     docker-compose pull
     docker-compose stop
     docker-compose rm -f
-    docker-compose up -d
+    docker-compose -f docker-compose.yml up -d
 EOF
 )
