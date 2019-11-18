52a68bb2b34e50c9e8452af7e3407deca47c466a
Damian F <damian.fikier@wp.pl>
2019-11-18 01:30:48 +0100

    Still fixing env

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,15 +8,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   ssh -v $DROPLET_USER@$DROPLET_IP -o StrictHostKeyChecking=no <<-EOF
     cd $SSH_PROJECT_FOLDER
     git pull
-    echo "export PGADMIN_LOGIN='$PGADMIN_LOGIN'"
-    echo "export PGADMIN_PWD='$PGADMIN_PWD'"
-    echo "export POSTGRES_DB=$POSTGRES_DB"
-    echo "export POSTGRES_USER=$POSTGRES_USER"
-    echo "export POSTGRES_PWD=$POSTGRES_PWD"
-    echo "export PGADMIN_PORT=$PGADMIN_PORT"
-    echo "export JWT_SECRET_KEY=$JWT_SECRET_KEY"
-    echo "export POSTGRES_PORT=5432"
-    echo "export POSTGRES_HOST=postgres"
+    echo "export PGADMIN_LOGIN=$PGADMIN_LOGIN" >> $BASH_ENV
+    echo "export PGADMIN_PWD=$PGADMIN_PWD" >> $BASH_ENV
+    echo "export POSTGRES_DB=$POSTGRES_DB" >> $BASH_ENV
+    echo "export POSTGRES_USER=$POSTGRES_USER" >> $BASH_ENV
+    echo "export POSTGRES_PWD=$POSTGRES_PWD" >> $BASH_ENV
+    echo "export PGADMIN_PORT=$PGADMIN_PORT" >> $BASH_ENV
+    echo "export JWT_SECRET_KEY=$JWT_SECRET_KEY" >> $BASH_ENV
+    echo "export POSTGRES_PORT=5432" >> $BASH_ENV
+    echo "export POSTGRES_HOST=postgres" >> $BASH_ENV
     docker-compose pull
     docker-compose stop
     docker-compose rm -f
